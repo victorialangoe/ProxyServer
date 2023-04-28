5cef580f3646778dd79b0a2dc3514cbc73d6b952
victorialangoe <89309733+victorialangoe@users.noreply.github.com>
2023-04-28 21:17:01 +0200

    added tcp_wait_timeout, done with connection.c

--- connection.c
+++ connection.c
@@ -20,8 +20,6 @@
 #include <netdb.h>
 // TODO: remove unused imports
 
-#define BUFFSIZE 255
-
 void check_error(int res, char *msg)
 {
     if (res == -1)
@@ -95,44 +93,47 @@ void tcp_close(int sock)
 
 int tcp_create_and_listen(int port)
 {
-    int socketfd,rc;
+    int socketfd, rc;
     struct sockaddr_in address;
 
-    socketfd = socket(AF_INET,SOCK_STREAM,0);
-    check_error(socketfd,"socket");
+    socketfd = socket(AF_INET, SOCK_STREAM, 0);
+    check_error(socketfd, "socket");
 
     address.sin_family = AF_INET;
     address.sin_port = htons(port);
     address.sin_addr.s_addr = INADDR_ANY;
 
-    rc = bind(socketfd,(struct sockaddr *)&address,sizeof(struct sockaddr_in));
-    check_error(rc,"bind");
+    rc = bind(socketfd, (struct sockaddr *)&address, sizeof(struct sockaddr_in));
+    check_error(rc, "bind");
 
-    rc = listen(socketfd,SOMAXCONN);
-    check_error(rc,"listen");
+    rc = listen(socketfd, SOMAXCONN);
+    check_error(rc, "listen");
 
     return socketfd;
 }
 
 int tcp_accept(int server_sock)
 {
-    int socketfd = accept(server_sock,NULL,NULL);
-    check_error(socketfd,"accept");
+    int socketfd = accept(server_sock, NULL, NULL);
+    check_error(socketfd, "accept");
 
     return socketfd;
-    
 }
 
 int tcp_wait(fd_set *waiting_set, int wait_end)
 {
-    int rc =select(wait_end+1,&waiting_set,NULL,NULL,NULL);
-    check_error(rc,"select");
+    int rc = select(wait_end + 1, &waiting_set, NULL, NULL, NULL);
+    check_error(rc, "select");
 
     return rc;
 }
 
 int tcp_wait_timeout(fd_set *waiting_set, int wait_end, int seconds)
 {
-    /* TO BE IMPLEMENTED */
-    return 0;
+    struct timeval timeval;
+    timeval.tv_sec = seconds;
+    int rc = select(wait_end + 1, &waiting_set, NULL, NULL, &timeval);
+    check_error(rc,"select");
+
+    return rc;
 }
